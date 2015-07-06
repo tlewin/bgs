3b585cd970aa6a9157ce964691a4fbf3c57a3015
Thiago Lewin <thiago_lewin@yahoo.com.br>
2015-07-06 00:04:12 -0300

    Play module structure

---
+++ lib/bgs/play.ex
@@ -0,0 +1,25 @@
+defmodule Play do
+  @moduledoc """
+  Encapsulates the game rules
+  """
+
+  @doc """
+  """
+  def move(board, play) do
+  end
+
+  @doc """
+  """
+  def can_double?(board, player) do
+    # if board.crawford_rule && board.crawford_match do
+    #   false
+    # elsif is_nil(board.nice)
+    #   true
+    # end
+  end
+
+  @doc """
+  """
+  def can_bear_off?(board, player) do
+  end
+end
